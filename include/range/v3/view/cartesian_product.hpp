ae272248f90d97c4539c1171481358b488482cfb
Casey Carter <Casey@Carter.net>
2017-01-13 11:10:48 -0800

    Copyright notices

--- include/range/v3/view/cartesian_product.hpp
+++ include/range/v3/view/cartesian_product.hpp
@@ -1,3 +1,17 @@
+/// \file
+// Range v3 library
+//
+//  Copyright Eric Niebler 2013-2014.
+//  Copyright Casey Carter 2017.
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/ericniebler/range-v3
+//
+
 #ifndef RANGES_V3_VIEW_CARTESIAN_PRODUCT_HPP
 #define RANGES_V3_VIEW_CARTESIAN_PRODUCT_HPP
 
--- test/view/cartesian_product.cpp
+++ test/view/cartesian_product.cpp
@@ -1,3 +1,17 @@
+/// \file
+// Range v3 library
+//
+//  Copyright Eric Niebler 2013-2014.
+//  Copyright Casey Carter 2017.
+//
+//  Use, modification and distribution is subject to the
+//  Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/ericniebler/range-v3
+//
+
 #include <iostream>
 #include <range/v3/utility/tuple_algorithm.hpp>
 #include <range/v3/view/cartesian_product.hpp>
